3d858b09ae9d9917004baea2b0f804c9355485e9
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-09-28 21:04:07 -0300

    Corrigindo Bug

--- src/main/java/br/com/fiap/mottomap/model/Area.java
+++ src/main/java/br/com/fiap/mottomap/model/Area.java
@@ -4,7 +4,7 @@ package br.com.fiap.mottomap.model;
 public enum Area {
     PRONTAS,            // Motos prontas para aluguel ou uso
     MINHA_MOTTU,        // Área destinada a motos do plano minha mottu
-    PROBLEMAS_SIMPLEs, // Motos com defeitos de fácil resolução
+    PROBLEMAS_SIMPLES, // Motos com defeitos de fácil resolução
     PROBLEMAS_GRAVES,   // Motos que necessitam de manutenção complexa
     IRRECUPERAVEIS      // Motos que foram baixadas ou não têm conserto
 }
