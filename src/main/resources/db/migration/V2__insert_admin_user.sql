cb05f1923102e1ed6224ca09988639a58a04d015
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-09-28 21:43:33 -0300

    Aprimorando as migrations do projeto

--- src/main/resources/db/migration/V2__insert_admin_user.sql
+++ src/main/resources/db/migration/V2__insert_admin_user.sql
@@ -1,11 +1,15 @@
-
+-- Inserindo uma filial padrão para o admin poder ser associado
 INSERT INTO filial
-    (nome, endereco, cidade, sigla_estado, numero_coluna, numero_linha, capacidade_maxima) VALUES
-    ('Sede Principal', 'Av. Faria Lima, 123', 'São Paulo', 'SP', 20, 10, 200);
+(id, nome, endereco, cidade, sigla_estado, numero_coluna, numero_linha, capacidade_maxima) VALUES
+    (1, 'Sede Principal', 'Av. Principal, 1', 'São Paulo', 'SP', 5, 10, 50)
+    ON CONFLICT (id) DO NOTHING;
 
+-- Inserindo o usuário ADM_GERAL (senha: "admin")
 INSERT INTO usuario
-    (nome, username, password, cargo_usuario, filial_id) VALUES
-    ('Admin Geral', 'admin@mottomap.com', '$2a$10$xSAFeotUwNG7FQ555d.q1etGWtyyaiR.EebJ/YStDAZl1zA9Qbg4i', 'ADM_GERAL', 1),
-    ('Admin Local', 'local@mottomap.com', '$2a$10$Ttw9WKwx/edi2vo1A5dNr.3.Q5q.biyEKMUsKdLLnY2mtk7.NlxbS', 'ADM_LOCAL', 1),
-    ('Colaborador Patio', 'patio@mottomap.com', '$2a$10$oxQHbr0CRyx6hQI4OKC8JOjIdnkUMizW2JbH0K93HH0mAvBCdp.Ba', 'COL_PATIO', 1),
-    ('Colaborador Mecanico', 'mecanico@mottomap.com', '$2a$10$HxEyBnJd7gBazPRf3tc4k.DpS.An2Z5rswqntT/mr9PICl2Mwl2wm', 'COL_MECANICO', 1);
+(id, nome, username, password, cargo_usuario, filial_id, ativo) VALUES
+    (1, 'Admin Geral', 'admin@mottomap.com', '$2a$10$xSAFeotUwNG7FQ555d.q1etGWtyyaiR.EebJ/YStDAZl1zA9Qbg4i', 'ADM_GERAL', 1, true)
+    ON CONFLICT (id) DO NOTHING;
+
+-- Recalculando as sequences para evitar conflitos
+SELECT setval('filial_id_seq', (SELECT MAX(id) FROM filial));
+SELECT setval('usuario_id_seq', (SELECT MAX(id) FROM usuario));
